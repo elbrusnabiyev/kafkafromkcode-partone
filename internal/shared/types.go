7b2467613705ea36dcc7112bc7b6ac8f408530de
Elbrus Nabiyev <elbrusnabiyev@gmail.com>
2026-03-17 18:04:56 +0500

    Next parttwo from kcode

--- internal/shared/types.go
+++ internal/shared/types.go
@@ -1,12 +1,27 @@
 package shared
 
-import "github.com/confluentinc/confluent-kafka-go/v2/kafka"
+import (
+	"encoding/json"
+	"fmt"
+
+	"github.com/confluentinc/confluent-kafka-go/v2/kafka"
+	"github.com/elbrusnabiyev/kafkafromkcode-partone/internal/repo"
+)
 
 type Message struct {
 	Metadata *kafka.TopicPartition
-	Event *repo.Event
+	Event    *repo.Event
 }
 
 func NewMessage(metadata *kafka.TopicPartition, data []byte) *Message {
-	
+	e := &repo.Event{}
+	err := json.Unmarshal(data, e)
+	if err != nil {
+		panic(fmt.Sprintf("error unmarshaling event = %v\n", err))
+	}
+
+	return &Message{
+		Metadata: metadata,
+		Event:    e,
+	}
 }
